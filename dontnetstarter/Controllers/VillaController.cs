6ff9056b52ef6c1a409483fa3dca1d2d002f4497
AdetolaAremu <user@MACs-MacBook-Pro.local>
2024-05-07 19:15:30 +0100

    getvilla patch

--- dontnetstarter/Controllers/VillaController.cs
+++ dontnetstarter/Controllers/VillaController.cs
@@ -4,6 +4,7 @@ using dontnetstarter.Models;
 using dontnetstarter.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using dontnetstarter.DataStore;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace dontnetstarter.Controllers
 {
@@ -43,8 +44,14 @@ namespace dontnetstarter.Controllers
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<VillaDTO> createVilla([FromBody] VillaDTO villaDto)
+		public ActionResult<VillaDTO> createVilla([FromBody] VillaDTO villaDto)
 		{
+			if (VillaData.villaList.FirstOrDefault(u => u.name.ToLower() == villaDto.name.ToLower()) != null)
+			{
+				ModelState.AddModelError("CustomError", "Villa already exsist");
+				return BadRequest(ModelState ); 
+			}
+
 			if (villaDto.id > 0)
 			{
 				return StatusCode(StatusCodes.Status400BadRequest);
@@ -61,5 +68,61 @@ namespace dontnetstarter.Controllers
 
 			return Ok(villaDto);
 		}
-	}
+
+		[HttpDelete("{id:int}")]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<VillaDTO> deleteVilla(int id)
+		{
+			var getVilla = VillaData.villaList.FirstOrDefault(u => u.id == id);
+
+			if (getVilla == null)
+			{
+				return NotFound();
+			}
+
+			VillaData.villaList.Remove(getVilla);
+			 
+			return Ok();
+		}
+
+		[HttpPut("{id:int}", Name = "UpdateVilla")]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public IActionResult updateVilla(int id, [FromBody]VillaDTO villa)
+		{
+			if (id == 0)
+			{
+				return BadRequest();
+			}
+
+			var getVilla = VillaData.villaList.FirstOrDefault(u => u.id == id);
+
+			if (getVilla == null) return NotFound();
+
+			getVilla.name = villa.name;
+
+			return Ok();
+		}
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+		public IActionResult updatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchVillaDTO)
+		{
+			if (patchVillaDTO == null || id == 0) return BadRequest();
+
+			var getVilla = VillaData.villaList.FirstOrDefault(u => u.id == id);
+
+			if (getVilla == null) return NotFound();
+
+			patchVillaDTO.ApplyTo(getVilla, ModelState);
+
+			if (!ModelState.IsValid) return BadRequest(ModelState);
+
+			return Ok();
+		}
+    }
 }
